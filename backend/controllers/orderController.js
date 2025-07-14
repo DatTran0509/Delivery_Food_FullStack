c7d754f83b1a115523f186291697bc271895983f
DatTran0509 <dat881070@gmail.com>
2025-07-14 17:12:36 +0700

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -16,7 +16,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //     console.error(err.message);
 // }
 const placeOrder = async (req, res) => {    
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "https://food-del-frontend-qek8.onrender.com"
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
